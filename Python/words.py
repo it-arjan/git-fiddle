89b833e383f21ccb9ba2a592c8bbc6932c7620c3
it-arjan <it-arjan@users.noreply.github.com>
2019-06-19 20:12:50 +0200

    refactor into module usable in REPL and program

--- Python/words.py
+++ Python/words.py
@@ -1,18 +1,29 @@
 from urllib.request import urlopen
+import sys
 
 
-def fetch_words():
-    with urlopen('http://sixty-north.com/c/t.txt') as story:
+def fetch_words(url):
+    with urlopen(url) as story:
         story_words = []
         for line in story:
             line_words = line.decode('utf-8').split()
             for word in line_words:
                 story_words.append(word)
+    return story_words
 
-    print(story_words)
+
+def print_items(items):
+    for item in items:
+        print(item)
+
+
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
 
 
 print(__name__)
 # When called as script ($ python words.py), execute fetch_words function of words module
 if __name__ == '__main__':
-    fetch_words()
+    url = sys.argv[1]
+    main(url)
